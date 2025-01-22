b04eecab201ff9171b4593788cfd362e672cd06f
suix0 <112095627+suix0@users.noreply.github.com>
2025-01-22 21:22:15 +0800

    fixed errors with modal

--- src/components/Electronics.jsx
+++ src/components/Electronics.jsx
@@ -1,24 +1,46 @@
 import { useOutletContext } from "react-router-dom";
+import { useState } from "react";
 import ProductCard from "./ProductCard";
+import CheckoutModal from "../utils/CheckoutModal";
 
 const Electronics = () => {
-  let products = useOutletContext();
+  const [selectedProductID, setSelectedProductID] = useState(null);
+
+  let { products, modal, setModal } = useOutletContext();
 
   if (products === null) {
     products = JSON.parse(localStorage.getItem("products"));
   }
 
+  const checkoutModal = (e) => {
+    setModal(true);
+    setSelectedProductID(e.target.id);
+  };
+
   return (
-    <div>
-      <h1 className="text-center">Electronics</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {products
-          .filter((product) => product.category === "electronics")
-          .map((product) => (
-            <ProductCard product={product} key={product.id} id={product.id} />
-          ))}
+    <>
+      <CheckoutModal
+        isOpen={modal}
+        closeModal={() => setModal(false)}
+        productID={selectedProductID}
+      ></CheckoutModal>
+      <div>
+        <h1 className="text-center">Everything we can offer</h1>
+        <div className="grid grid-cols-4 gap-4">
+          {products
+            .filter((product) => product.category === "electronics")
+            .map((product) => (
+              <ProductCard
+                product={product}
+                key={product.id}
+                id={product.id}
+                clickHandler={checkoutModal}
+                isModalOpen={modal}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Jewelery.jsx
+++ src/components/Jewelery.jsx
@@ -1,24 +1,46 @@
 import { useOutletContext } from "react-router-dom";
+import CheckoutModal from "../utils/CheckoutModal";
+import { useState } from "react";
 import ProductCard from "./ProductCard";
 
 const Jewelery = () => {
-  let products = useOutletContext();
+  const [selectedProductID, setSelectedProductID] = useState(null);
+
+  let { products, modal, setModal } = useOutletContext();
 
   if (products === null) {
     products = JSON.parse(localStorage.getItem("products"));
   }
 
+  const checkoutModal = (e) => {
+    setModal(true);
+    setSelectedProductID(e.target.id);
+  };
+
   return (
-    <div>
-      <h1 className="text-center">Jewelery</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {products
-          .filter((product) => product.category === "jewelery")
-          .map((product) => (
-            <ProductCard product={product} key={product.id} id={product.id} />
-          ))}
+    <>
+      <CheckoutModal
+        isOpen={modal}
+        closeModal={() => setModal(false)}
+        productID={selectedProductID}
+      ></CheckoutModal>
+      <div>
+        <h1 className="text-center">Everything we can offer</h1>
+        <div className="grid grid-cols-4 gap-4">
+          {products
+            .filter((product) => product.category === "jewelery")
+            .map((product) => (
+              <ProductCard
+                product={product}
+                key={product.id}
+                id={product.id}
+                clickHandler={checkoutModal}
+                isModalOpen={modal}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
